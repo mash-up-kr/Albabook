5ed3ec267142cd77bf2f200ae12d16528428e7b3
aereeeee <dofl5576@gmail.com>
2019-11-24 05:03:43 +0900

    style: 버튼 스타일 수정

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import store from './store/configureStore';
+// import store from './store/configureStore';
 
 const Root = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  // <Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </Provider>
 );
 
 export default Root;
--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import KakaoAuth from 'react-kakao-login';
 
+const S = {
+  KakaoButton: styled(KakaoAuth)`
+    position:absolute;
+    bottom:50px;
+    left:50%;
+    transform:translateX(-50%);
+  `,
+};
 const LoginButton = () => {
   const handleResponse = async (res) => {
     const token = res.response.access_token;
@@ -15,7 +24,7 @@ const LoginButton = () => {
   };
 
   return (
-    <KakaoAuth
+    <S.KakaoButton
       jsKey={process.env.REACT_APP_KAKAO_ID}
       getProfile
       useDefaultStyle
